967dbc7495b44741dffd569d2cbc94b51c776df5
Veianya Hansen <melaya0105@gmail.com>
2021-02-03 10:45:04 -0700

    Completed

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,33 +6,64 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
+
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true
+            };
+
+            var myAnimals = new Animal[] { myBird, lizard };
+
+            foreach (var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has {animal.LegCount} legs");
+                Console.WriteLine($"It lives by: {animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+            
+                
+
+            
+
+
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
+            // Create a class Animal - DONE
+            // give this class 4 members that all Animals have in common - DONE
 
 
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
+            // Create a class Bird - DONE
+            // give this class 4 members that are specific to Bird - DONE
+            // Set this class to inherit from your Animal Class - DONE
 
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
-
+            // Create a class Reptile - DONE
+            // give this class 4 members that are specific to Reptile - DONE
+            // Set this class to inherit from your Animal Class - DONE
 
 
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
+
+
+            /*Create an object of your Bird class - DONE
+             *  give values to your members using the object of your Bird class - DONE
              *  
-             * Creatively display the class member values 
+             * Creatively display the class member values - DONE
              */
 
             /*Create an object of your Reptile class
-             *  give values to your members using the object of your Bird class
+             *  give values to your members using the object of your Bird class - DONE
              *  
-             * Creatively display the class member values 
+             * Creatively display the class member values - DONE
              */
         }
     }
